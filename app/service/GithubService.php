64a34584c794e139f0ddd876c8f38cbca481b17d
Marc <mcalafellsmx@gmail.com>
2016-11-15 20:39:41 +0100

    end of class

--- app/service/GithubService.php
+++ app/service/GithubService.php
@@ -9,20 +9,41 @@ namespace App\service;
  * Time: 19:54
  */
 class GithubService {
-protected $token;
+    protected $token;
+    protected $guzzle;
+
+    protected $github_api_url="https://api.github.com";
+
+    protected $uri= '/user/repos';
 
     public function __construct() {
         $this->token = env('GITHUB_TOKEN');
     }
 
     public function obtainrepos() {
+        $res= $this->guzzle->request('GET',$this->gitHubApiUrl(),
+
+            [
+                "auth"=> $this->credentials()
+
+            ]
+
+
+            );
+
+        dd($res->getBody());
+
 
 
     }
 
-//        $mygithubrepos=[
-//            'repos1'=>'asd',
-//            'repos2'=>'qwerty',
-//            'repos3'=>'asdf'
-//       ];
+    private function gitHubApiUrl() {
+        return $this->github_api_url . $this->uri;
+    }
+
+    private function credentials()   {
+
+    }
+
+
 }
